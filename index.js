88ca5b8080cfb51fc1fc935a57c70cf5bcf694a7
felipesmith <fsmith95@hotmail.com>
2020-10-01 23:05:31 -0300

    Parse ES6 to ES5 with Babel

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
@@ -10,38 +11,25 @@ let clients = [];
 app.use(express.static(__dirname + "/"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 io.on("connect", (socket) => {
   clients.push(socket);
-  console.log(
-    "Connection succeeded for client: " +
-      socket.id +
-      " A.K.A: " +
-      clients.length
-  );
   socket.on("disconnect", () => {
     console.log(clients.indexOf(socket));
     if (clients.indexOf(socket) != -1) {
       clients.splice(clients.indexOf(socket), 1);
     }
-    console.log("Connections Remaining: " + clients.length);
   });
 
   socket.on("play", (data) => {
-    console.log(
-      "The user " + socket.id + " requested to play the video at " + data.time
-    );
     socket.broadcast.emit("play", {
       time: data.time,
     });
   });
 
   socket.on("pause", (data) => {
-    console.log(
-      "The user " + socket.id + " requested to pause the video at " + data.time
-    );
     socket.broadcast.emit("pause", {
       time: data.time,
     });
---
+++ src/video.js
@@ -0,0 +1,76 @@
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var video = document.querySelector("video");
+var playerStatus = -1;
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    height: "360",
+    width: "640",
+    videoId: "dQw4w9WgXcQ",
+    playerVars: { loop: 1, origin: "https://videomatch.herokuapp.com/" },
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+    },
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+  event.target.pauseVideo();
+}
+
+var playerState = -1;
+function onPlayerStateChange(event) {
+  var currentTime = event.target.getCurrentTime();
+  if (
+    event.data == YT.PlayerState.PLAYING &&
+    playerState != YT.PlayerState.PLAYING
+  ) {
+    playVideo(currentTime);
+    playerState = YT.PlayerState.PLAYING;
+  } else if (
+    event.data == YT.PlayerState.PAUSED &&
+    playerState != YT.PlayerState.PAUSED
+  ) {
+    pauseVideo(currentTime);
+    playerState = YT.PlayerState.PAUSED;
+  } else if (event.data == YT.PlayerState.ENDED) {
+    player.playVideo();
+    playerState = YT.PlayerState.PLAYING;
+  }
+}
+
+const socket = io.connect();
+
+window.onclose = function () {
+  socket.disconnect();
+};
+
+socket.on("play", (data) => {
+  player.seekTo(data.time);
+  player.playVideo();
+});
+
+socket.on("pause", (data) => {
+  //player.seekTo(data.time);
+  player.pauseVideo();
+});
+
+function playVideo(currentTime) {
+  socket.emit("play", {
+    time: currentTime,
+  });
+  player.playVideo();
+}
+
+function pauseVideo(currentTime) {
+  socket.emit("pause", {
+    time: currentTime,
+  });
+  player.pauseVideo();
+}
